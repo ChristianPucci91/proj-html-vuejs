9ba4da268cb122ee8c8f8fbe62027cec4c8f0e95
ChristianPucci91 <pucci.christian91@gmail.com>
2020-12-09 19:07:53 +0100

    bozza section1 completata

--- js/script.js
+++ js/script.js
@@ -3,13 +3,21 @@ var app = new Vue({
   el: "#app",
   data: {
    logo:"img/logo.png",
-   navBar:[
+   navBar:[ // array link navbar
      {section:"Home"},
      {section:"Courses"},
      {section:"Intructors"},
      {section:"Events"},
      {section:"Pages"},
      {section:"Elements"}
+   ],
+   section1:[ //icone e testo section 1
+     {img:"img/h5-custom-icon-1.png",text:"Languages"},
+     {img:"img/h5-custom-icon-2.png",text:"Software"},
+     {img:"img/h5-custom-icon-3.png",text:"Business"},
+     {img:"img/h5-custom-icon-4.png",text:"Chemistry"},
+     {img:"img/h5-custom-icon-5.png",text:"Science"},
+     {img:"img/h5-custom-icon-6.png",text:"DIY&Craft"}
    ]
   }
 })
