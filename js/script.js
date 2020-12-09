50a9c35efa2c3c6e04be625a727d9b42f33e5797
ChristianPucci91 <pucci.christian91@gmail.com>
2020-12-09 16:29:25 +0100

    iniziando a settare la sezione header con jumbotron

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
 // test JS
-console.log("testJS");
+var app = new Vue({
+  el: "#app",
+  data: {
+   logo:"img/logo.png",
+   navBar:[
+     {section:"Home"},
+     {section:"Courses"},
+     {section:"Intructors"},
+     {section:"Events"},
+     {section:"Pages"},
+     {section:"Elements"}
+   ]
+  }
+})
