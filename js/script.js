8916bcfab33eaf711cd1180e8434c81e6be8c34f
ChristianPucci91 <pucci.christian91@gmail.com>
2020-12-10 01:50:09 +0100

    bozza footer/header in vue js completato

--- js/script.js
+++ js/script.js
@@ -26,7 +26,52 @@ var app = new Vue({
      {text:"Personal Managment"},
      {text:"Steps To Success"},
      {text:"Knowledge Transfer"},
-   ]
+   ],
+   footer1:[ //Footer 1 colonna
+     {
+       img:"img/logo.png",
+       info:[
+         {text:"Providing Life Changing"},
+         {text:"Experiences Through"},
+         {text:"Education. Class That Fit Your"},
+         {text:"Busy Life. Closer to Home"}
+       ],
+       contact:"1-677-124-44227",
+       time:"Mon - Sat 8.00 - 18.00"
+     }
+   ],
+   footer2:[ //Footer 2 colonna
+     {
+       title:"Popular Courses",
+       info:[
+         {text:"Business English",name:"Preston Marshall"},
+         {text:"Social Computing",name:"David Sanders"},
+         {text:"Learn Spanish",name:"Jannie King"}
+       ]
+     }
+   ],
+   footer3:[ // Footer 3 colonna
+     {
+       title:"Support",
+       info:[
+         {text:"User dashboard"},
+         {text:"Contact Us"},
+         {text:"FAQ"},
+         {text:"Course Offer"},
+         {text:"Events"}
+       ]
+     }
+   ],
+   footer4:[ // Footer 4 colonna
+     {title:"Flexible Learning",img:"img/footer-img-1.png"}
+   ],
 
+   call:"+44 300 303 0266", // info footer
+
+   social:[ // icon footer
+     {icon:"fa-twitter"},
+     {icon:"fa-instagram"},
+     {icon:"fa-facebook"}
+   ]
   }
 })
